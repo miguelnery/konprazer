17cd2554bb06e7b18fc24950ba0e9900c7b8200b
Miguel Nery <nery.mik@gmail.com>
2020-08-24 23:25:56 -0300

    Initial Commit

---
+++ Konprazer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Konprazer
+//
+//  Created by Miguel Nery on 24/08/20.
+//  Copyright © 2020 Miguel Nery. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
